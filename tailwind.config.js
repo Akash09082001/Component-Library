eec8f7b0de5c1f0ef7cd47505fa46a102944570d
Akash lakade <130669267+Akash09082001@users.noreply.github.com>
2023-09-26 11:55:06 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/style.css",
+    "./public/**/*.js",
+    "./public/assets/",
+  ],
+  theme: {
+    screens: {
+
+      'xs' :'350px',
+      // => @media (min-width: 350px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors :{
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
+    extend: {
+      backgroundImage: {
+        'hero-banner': 'url(assets/img/bg.png)'
+      },
+    },
+  },
+  plugins: [],
+}
+
