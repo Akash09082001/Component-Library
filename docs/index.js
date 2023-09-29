82b23c17e35f728086dd5b515cd3a69e2964a5f7
Akash lakade <130669267+Akash09082001@users.noreply.github.com>
2023-09-29 18:03:41 +0530

    fixed some bugs

--- docs/index.js
+++ docs/index.js
@@ -4,19 +4,23 @@ function toggleContent(contentType) {
     let previewContent = document.getElementById('previewContent');
     let htmlContent = document.getElementById('htmlContent');
     let javascriptContent = document.getElementById('javascriptContent');
-    
+    let cardStatus = document.getElementById('card-status');
+
     if (contentType === 'preview') {
         previewContent.style.display = 'flex';
         htmlContent.style.display = 'none';
         javascriptContent.style.display = 'none';
+        cardStatus.innerText= "Preview";
     } else if (contentType === 'html') {
         previewContent.style.display = 'none';
         htmlContent.style.display = 'flex';
         javascriptContent.style.display = 'none';
+        cardStatus.innerText= "HTML + Tailwind CSS";
     }else if(contentType === 'javascript') {
         previewContent.style.display = 'none';
         htmlContent.style.display = 'none';
         javascriptContent.style.display = 'flex';
+        cardStatus.innerText= "Javascript";
     }
 
 };
